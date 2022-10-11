d588fa93c9057f92cdb64fbe8a50c8247b5b27de
Manuel <103332363+ManuAnria@users.noreply.github.com>
2022-10-11 13:30:36 +0200

    L1

---
+++ 1-Introduccion/intro.js
@@ -0,0 +1,3 @@
+// Este es un mensaje de bienvenida
+/* Esta es otra forma de hacer comentarios*/
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo");
