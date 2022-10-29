640e19cfeaab2dc0b4b5acc114c5f7d887ec8a35
Manuel <103332363+ManuAnria@users.noreply.github.com>
2022-10-29 12:48:09 +0200

    Update ejercicio.js

--- 5-Numbers/ejercicio.js
+++ 5-Numbers/ejercicio.js
@@ -2,8 +2,8 @@ let altura_cm = 186;
 let altura_mt = 1.86;
 let peso = 80.4;
 
-let alt_round = Math.round(altura_mt);
-let peso_round = Math.round(peso);
+let alt_round = Math.ceil(altura_mt);
+let peso_round = Math.floor(peso);
 
-let comp = (Number.MAX_VALUE + 1) == Number.MAX_VALUE;
+let comp = (Number.MAX_VALUE + 1) === Number.MAX_VALUE;
 console.log(comp);
